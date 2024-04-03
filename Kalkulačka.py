6d7cd6a6aee87bd8297a57ad2e46e78f0de08e5b
jakubholan <159123546+jakubholan@users.noreply.github.com>
2024-04-03 10:08:18 +0200

    Update Kalkulačka.py

--- Kalkulačka.py
+++ Kalkulačka.py
@@ -1,1 +1,24 @@
-
+print ("Vítejte v programu kalkulačka, jestliže chcete program ukončit napište ‘konec‘ do operace.")
+while True:
+    cislo1 = float(input("Zadejte první číslo:"))
+    cislo2 = float(input("Zadejte druhé číslo:"))
+    operace = input("Zadejte operaci +, -, *, /, **, //")
+    if operace == "+":
+        print(cislo1 + cislo2)
+    elif operace == "-":
+        print(cislo1 - cislo2)
+    elif operace == "*":
+        print(cislo1 * cislo2)
+    elif (cislo1 == 0 or cislo2 == 0) and operac == "/":
+        print("Nulou nelze dělit")
+    elif operace == "/":
+        print(cislo1 / cislo2)
+    elif operace == "**":
+        print(cislo1 ** cislo2)
+    elif operace == "//"
+        print(cislo1 // cislo2)
+    elif operace == "konec":
+        print("Děkujeme za využití programu kalkulačka.")
+        break
+    else:
+        print("Proč to tam vlastně píšeš")
